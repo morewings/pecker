be048362ae17633c999d048363ad1b48c7093eb4
Dima Vishnyakov <morewings@gmail.com>
2015-05-08 00:21:15 +0300

    modals

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
    //nav
-    var mainHeight=$('#main').height(),
-        navMenu=$('.navmenu');
-    console.log(mainHeight);
+    var navMenu=$('.navmenu');
     $('.custom-navbar-toggle').on('click', function(e){
         e.preventDefault();
         navMenu.toggleClass('show-menu')
