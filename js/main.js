ab22261472661ed47fe4a6584e8f0408ae8147ac
Dima Vishnyakov <morewings@gmail.com>
2015-05-07 21:24:16 +0300

    tired of this shit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+   //nav
+    var mainHeight=$('#main').height(),
+        navMenu=$('.navmenu');
+    console.log(mainHeight);
+    $('.custom-navbar-toggle').on('click', function(e){
+        e.preventDefault();
+        navMenu.toggleClass('show-menu')
+    })
+});
