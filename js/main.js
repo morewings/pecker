a2dc1fd87c504ba7068b28ee1e3dadff5c112492
Dima Vishnyakov <morewings@gmail.com>
2015-05-12 20:20:36 +0300

    video support

--- js/main.js
+++ js/main.js
@@ -31,4 +31,94 @@ $(document).ready(function(){
             scrollCheck();
         });
     }
+
+    //video load
+
+
+    var VideoTabs ={
+        playing:{},
+
+        init: function(){
+            VideoTabs.findVideo();
+            VideoTabs.clickWatch();
+        },
+
+        build: function(targetVideo,targetTabName){
+            targetVideo
+                .attr({
+                    'src': './video/'+ targetTabName.substring(1) + '/video.'+VideoTabs.browserComp(),
+                    'type': 'video/webm; codecs="vp8, vorbis"',
+                    'preload': 'auto'
+                });
+
+            //console.log('build triggered');
+            VideoTabs.playVideo(targetVideo);
+
+        },
+
+        browserComp: function () {
+            if(Modernizr.video.webm){
+                return 'webm'
+            }
+            else{
+                return 'mp4'
+            }
+        },
+
+        playVideo: function(targetVideo){
+
+            targetVideo
+                .load()
+                .on('canplaythrough', function(){
+                    $(this).addClass('playing').get(0).play();
+
+                });
+
+            VideoTabs.playing = targetVideo;
+
+            //console.log('playVideo triggered');
+            //console.log(targetVideo);
+            //console.log(VideoTabs.playing);
+        },
+
+        findVideo: function(){
+            var targetTabName = $('#collapse-tabs').find('.active a').attr('href'),
+                targetVideo = $(targetTabName).find('video');
+
+
+            VideoTabs.build(targetVideo,targetTabName);
+
+        },
+
+        clickWatch: function(){
+            $('#collapse-tabs').on('click', 'a', function(){
+                var targetTabName = $(this).attr('href'),
+                    targetVideo = $(targetTabName).find('video');
+                VideoTabs.destroyVideo(VideoTabs.playing);
+                VideoTabs.build(targetVideo,targetTabName);
+            });
+            //console.log('clickWatch triggered');
+        },
+
+
+        destroyVideo: function (targetVideo) {
+            targetVideo.get(0).pause();
+            targetVideo.attr({
+                'src': ''
+            });
+            targetVideo.get(0).load();
+            targetVideo.removeAttr('src');
+            //console.log('destroy triggered');
+
+        }
+    };
+
+    if(!Modernizr.touch){
+        VideoTabs.init();
+    }
+
+
+
+
+
 });
